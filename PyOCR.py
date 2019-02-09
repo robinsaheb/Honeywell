82cc2f0fe19fbf9e449e676fb27a5a54673c5a6a
ashishkumar4 <ashishkmr472@gmail.com>
2019-02-09 13:02:44 +0530

    OCR Classes Added

---
+++ PyOCR.py
@@ -0,0 +1,19 @@
+import os 
+import Image
+import pytesseract
+
+class PyOCR:
+    def __init__(self, image, preprocess=True):
+        if(preprocess):
+            os.system('python3 ImagePreprocess.py ' + image + ' proc_' + image)
+            image = 'proc_' + image 
+        self.img_name = image
+        self.text = ''
+    def toText(self, method='tesseract'):
+        if method == 'tesseract':
+            self.text = pytesseract.image_to_string(Image.open(self.img_name))
+        else if method == 'algorithmia_smartOCR'
+            client = Algorithmia.client('simwMUzTtxRs30OR13kELwpYPnM1')
+            algo = client.algo('ocr/SmartOCR/0.2.6')
+            self.text = algo.pipe(self.img_name).result
+        return self.text
